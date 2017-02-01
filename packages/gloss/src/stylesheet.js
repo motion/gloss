7d6b00b2b6050c41cf4e4dfc538bec3f688fb060
natew <natewienert@gmail.com>
2017-01-31 22:44:13 -0800

    :art: simpler

--- packages/gloss/src/stylesheet.js
+++ packages/gloss/src/stylesheet.js
@@ -2,7 +2,4 @@ import aphrodisiac from 'aphrodisiac'
 import preset from 'jss-preset-default'
 import { create } from 'jss'
 
-const Aphrodisiac = aphrodisiac(create(preset()))
-
-export const StyleSheet = Aphrodisiac.StyleSheet
-export const css = Aphrodisiac.css
+export const { css, StyleSheet } = aphrodisiac(create(preset()))
