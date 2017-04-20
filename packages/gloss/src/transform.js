99797256a42686b48eafc2ebb6e193473449fccb
natew <natewienert@gmail.com>
2017-04-20 11:35:58 -0700

    :new: add support for @style() decorators

--- packages/gloss/src/transform.js
+++ packages/gloss/src/transform.js
@@ -4,9 +4,11 @@ export default function({ types: t }: { types: Object }) {
   const classBodyVisitor = {
     ClassMethod(path: Object) {
       if (path.node.key.name === 'render') {
+        // add a fancyelement hook to start of render
         path.node.body.body.unshift(
           t.expressionStatement(
-            t.assignmentExpression('=',
+            t.assignmentExpression(
+              '=',
               t.memberExpression(
                 t.identifier('React'),
                 t.identifier('createElement')
@@ -16,7 +18,7 @@ export default function({ types: t }: { types: Object }) {
           )
         )
       }
-    }
+    },
   }
 
   return {
@@ -28,34 +30,54 @@ export default function({ types: t }: { types: Object }) {
         }
 
         // -- Validate if class is what we're looking for
-        // Default to @style
-        const decoratorName = state.opts.decoratorName || 'style'
-        const isMotionStyle = node.decorators.some(item => {
+        //    has some flexibility, looks for any of:
+        //       @x  @x()  @x.y  @x.y()
+
+        const decoratorName =
+          (state.opts && state.opts.decoratorName) || 'style'
+
+        const foundDecorator = node.decorators.some(item => {
           if (!item.expression) {
             return false
           }
-          if (item.expression.type === 'Identifier' && item.expression.name === decoratorName) {
+          // @style
+          if (
+            item.expression.type === 'Identifier' &&
+            item.expression.name === decoratorName
+          ) {
             return true
           }
+          // @style()
           if (
-            item.expression.callee && t.isMemberExpression(item.expression.callee) &&
+            item.expression.callee &&
+            item.expression.callee.name === decoratorName
+          ) {
+            return true
+          }
+          // @style.something()
+          if (
+            item.expression.callee &&
+            t.isMemberExpression(item.expression.callee) &&
             item.expression.callee.object.name === decoratorName
           ) {
             return true
           }
+          // @style.something
           if (
-            item.expression.object && item.expression.object.name === decoratorName
+            item.expression.object &&
+            item.expression.object.name === decoratorName
           ) {
             return true
           }
 
           return false
         })
+
         // -- Add a unique var to scope and all of JSX elements
-        if (isMotionStyle) {
+        if (foundDecorator) {
           path.traverse(classBodyVisitor, state)
         }
-      }
-    }
+      },
+    },
   }
 }
