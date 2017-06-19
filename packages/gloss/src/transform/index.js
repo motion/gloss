91c3194cd8756b4c4e56f35f3a2228acec65b76a
natew <natewienert@gmail.com>
2017-06-19 15:26:38 -0700

    :bug: make gloss work on new versions of babel

--- packages/gloss/src/transform/index.js
+++ packages/gloss/src/transform/index.js
@@ -77,62 +77,68 @@ export default function({ types: t }: { types: Object }) {
     },
   }
 
+  const programVisitor = {
+    Class(path: Object, state: Object) {
+      const node = path.node
+
+      if (!node.decorators || !node.decorators.length) {
+        return
+      }
+
+      // -- Validate if class is what we're looking for
+      //    has some flexibility, looks for any of:
+      //       @x  @x()  @x.y  @x.y()
+
+      const decoratorName = (state.opts && state.opts.decoratorName) || 'style'
+
+      const foundDecorator = node.decorators.some(item => {
+        if (!item.expression) {
+          return false
+        }
+        // @style
+        if (
+          item.expression.type === 'Identifier' &&
+          item.expression.name === decoratorName
+        ) {
+          return true
+        }
+        // @style()
+        if (
+          item.expression.callee &&
+          item.expression.callee.name === decoratorName
+        ) {
+          return true
+        }
+        // @style.something()
+        if (
+          item.expression.callee &&
+          t.isMemberExpression(item.expression.callee) &&
+          item.expression.callee.object.name === decoratorName
+        ) {
+          return true
+        }
+        // @style.something
+        if (
+          item.expression.object &&
+          item.expression.object.name === decoratorName
+        ) {
+          return true
+        }
+
+        return false
+      })
+
+      // -- Add a unique var to scope and all of JSX elements
+      if (foundDecorator) {
+        path.traverse(classBodyVisitor, state)
+      }
+    },
+  }
+
   return {
     visitor: {
-      ClassExpression(path: Object, state: Object) {
-        const node = path.node
-        if (!node.decorators || !node.decorators.length) {
-          return
-        }
-
-        // -- Validate if class is what we're looking for
-        //    has some flexibility, looks for any of:
-        //       @x  @x()  @x.y  @x.y()
-
-        const decoratorName =
-          (state.opts && state.opts.decoratorName) || 'style'
-
-        const foundDecorator = node.decorators.some(item => {
-          if (!item.expression) {
-            return false
-          }
-          // @style
-          if (
-            item.expression.type === 'Identifier' &&
-            item.expression.name === decoratorName
-          ) {
-            return true
-          }
-          // @style()
-          if (
-            item.expression.callee &&
-            item.expression.callee.name === decoratorName
-          ) {
-            return true
-          }
-          // @style.something()
-          if (
-            item.expression.callee &&
-            t.isMemberExpression(item.expression.callee) &&
-            item.expression.callee.object.name === decoratorName
-          ) {
-            return true
-          }
-          // @style.something
-          if (
-            item.expression.object &&
-            item.expression.object.name === decoratorName
-          ) {
-            return true
-          }
-
-          return false
-        })
-
-        // -- Add a unique var to scope and all of JSX elements
-        if (foundDecorator) {
-          path.traverse(classBodyVisitor, state)
-        }
+      Program(path: Object, state: Object) {
+        path.traverse(programVisitor, state)
       },
     },
   }
